0dccc74501f117a34e75d27dce3824cb002fbd42
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2025-03-13 11:05:39 +0100

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    //fmt::print("Hello World!\n");
-    //fmt::print("We are not there yet...\n");
+    fmt::print("Hello World!\n");
+    fmt::print("We are not there yet...\n");
     return 0;
 }
